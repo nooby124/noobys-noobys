e35d4a1b328d483f02b4ebb4ac5ffcb033318bfa
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-07-19 22:24:38 +0300

    since when did github had emojis :skull: :happy: :skull:

--- wesbite.py
+++ wesbite.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import random
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+
+random_github_project = ['https://github.com/nooby124/Nooby-s-calculator', 'https://github.com/nooby124/Your-mother--game-',]
+random_github_project_real = random.choice(random_github_project)
+
+
+st.title('random one-of-my-github-projects generator')
+
+
+if st.button('generate'):
+    st.write(float(random_github_project))
+else:
+    st.empty

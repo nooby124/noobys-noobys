c7ee1c4e9f7dee53b3ced592f1d9ce331b0e2cf7
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-07-19 22:27:42 +0300

    the real version is not an imposter :skull: :skull:

--- wesbite.py
+++ wesbite.py
@@ -12,7 +12,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 random_github_project = ['https://github.com/nooby124/Nooby-s-calculator', 'https://github.com/nooby124/Your-mother--game-',]
-random_github_project_real = random.choice(random_github_project)
+random_github_project_real = random.choice(random_github_project) #the real version is not an imposter
 
 
 st.title('random one-of-my-github-projects generator')

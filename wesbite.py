a6be48488bbd860265fd334acb14f81cff5dec02
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-08-22 18:26:20 +0300

    small change

--- wesbite.py
+++ wesbite.py
@@ -11,7 +11,7 @@ def buttonshit():
 
 
 
-random_github_project = random_github_project = ['https://github.com/nooby124/Nooby-s-calculator', 'https://github.com/nooby124/Your-mother--game-', 'https://github.com/nooby124/noobys-stuff-2', 'https://github.com/nooby124/noobys-website-for-a-very-specific-purpose', 'https://github.com/nooby124/noobys-fake-nitro-gen', 'https://github.com/nooby124/noobys-noobys', '[https://github.com/noobys-off](https://youtu.be/uHgt8giw1LY)']
+random_github_project = random_github_project = ['https://github.com/nooby124/Nooby-s-calculator', 'https://github.com/nooby124/Your-mother--game-', 'https://github.com/nooby124/noobys-stuff-2', 'https://github.com/nooby124/noobys-website-for-a-very-specific-purpose', 'https://github.com/nooby124/noobys-fake-nitro-gen', 'https://github.com/nooby124/noobys-noobys', '[https://github.com/noobys-but-off](https://youtu.be/uHgt8giw1LY)']
 random_github_project_real = random.choice(random_github_project) #the real version is not an imposter
 
 
